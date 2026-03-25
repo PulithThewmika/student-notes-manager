c3266242e5d299b5e98b1c2979cfe99dc4790e31
IT23656338 <it23656338@my.sliit.lk>
2026-03-25 15:18:25 +0530

    feat: Add Welcome Higher-Order Component to manage welcome screen display

---
+++ React.js front-end/src/WelcomeHOC.jsx
@@ -0,0 +1,20 @@
+﻿import { useState, useEffect } from "react";
+import NovaNotesWelcome from "./NovaNotesWelcome";
+
+export function withWelcome(Component) {
+  return function WrappedComponent(props) {
+    const [started, setStarted] = useState(false);
+    useEffect(() => {
+        const hasStarted = sessionStorage.getItem("welcomePlayed");
+        if(hasStarted) {
+            setStarted(true);
+        }
+    }, [])
+    if(started) {
+        return <Component {...props} />
+    } else {
+        return <NovaNotesWelcome onEnter={() => { setStarted(true); sessionStorage.setItem("welcomePlayed", "true") }} />
+    }
+  }
+}
+

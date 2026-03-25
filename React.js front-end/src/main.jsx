977749e62203d3f468f7978e61cd293c6b51693b
IT23656338 <it23656338@my.sliit.lk>
2026-03-25 12:37:38 +0530

    fix: Update route to use NotesUi component instead of App in main.jsx

--- React.js front-end/src/main.jsx
+++ React.js front-end/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
+import NotesUi from './NotesUi.jsx'
 import LandingPage from './LandingPage.jsx'
 import LoginPage from './LoginPage.jsx'
 import RegisterPage from './RegisterPage.jsx'
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/app" element={<App />} />
+        <Route path="/app" element={<NotesUi />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
